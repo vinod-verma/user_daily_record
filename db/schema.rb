7de42a1782a243711064ca9ee9dc22bc2705cf9c
vinod-verma <vi.vermad@gmail.com>
2024-01-28 09:35:30 +0530

    configuration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_26_111415) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_23_063417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_26_111415) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "full_name"
   end
 
 end
